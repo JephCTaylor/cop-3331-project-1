fcac1c059d37d1a7a6c67d60e4cdeb84dd0b0685
JephCTaylor <jctaylor@mail.usf.edu>
2020-02-13 16:21:55 -0500

    added user input rolls

--- project_1_jefferson_taylor/project_1_jefferson_taylor.cpp
+++ project_1_jefferson_taylor/project_1_jefferson_taylor.cpp
@@ -7,19 +7,20 @@
 
 int main()
 {
-	int user_seed;
+	int user_seed, rolls;
 
 	std::cout << "Enter a seed value for the dice rolls: " << std::endl;
 	std::cin >> user_seed;
-	std::cout << std::endl;
-
+	std::cout << "Enter amount of rolls: " << std::endl;
+	std::cin >> rolls;
 	srand(user_seed);
+	std::cout << std::endl;
 
 	Dice dice;
 	Histogram histo;
 	GameStats stats;
 
-	stats.displayDiceStats(1, 6000, dice, histo);
-	stats.displayDiceStats(2, 6000, dice, histo);
-	stats.displayDiceStats(3, 6000, dice, histo);
+	stats.displayDiceStats(1, rolls, dice, histo);
+	stats.displayDiceStats(2, rolls, dice, histo);
+	stats.displayDiceStats(3, rolls, dice, histo);
 }
